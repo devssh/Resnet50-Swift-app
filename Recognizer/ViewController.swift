eb7cca0d0184866a94b35acea19b694e1e4be857
devssh <devasood@gmail.com>
2017-12-28 14:58:16 +0530

    Initial Commit

---
+++ Recognizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recognizer
+//
+//  Created by devssh on 28/12/17.
+//  Copyright © 2017 devssh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
